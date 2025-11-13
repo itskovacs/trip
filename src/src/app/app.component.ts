643810a36946b7fdd7a6fe90c8a730a9313b79a9
itskovacs <itskovacs@proton.me>
2025-11-13 22:29:46 +0100

    :art: remove unused import

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {} from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
