6e73b528ed73e851b5dd988ee72ddf5c5b7dfac2
itskovacs <itskovacs@proton.me>
2026-03-08 14:55:31 +0100

    :lipstick: Remove BETA TRIP UI modal

--- src/src/app/components/trip/trip.component.ts
+++ src/src/app/components/trip/trip.component.ts
@@ -199,7 +199,6 @@ export class TripComponent implements AfterViewInit, OnDestroy {
   isMembersDialogVisible = false;
   isAttachmentsDialogVisible = false;
   isChecklistDialogVisible = false;
-  isBetaDialogVisible = true;
   selectedItemProps = signal<string[]>(['place', 'comment', 'price']);
 
   tripSharedDetails$?: Observable<SharedTripDetails>;
