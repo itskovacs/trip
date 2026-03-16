c1e5028a5d046f0bbda9545d6e076d47549ae747
itskovacs <itskovacs@proton.me>
2026-03-16 17:42:19 +0100

    :fire: remove orphaned images on start

--- backend/trip/db/migrations.py
+++ backend/trip/db/migrations.py
@@ -10,13 +10,38 @@ from ..utils.utils import backup_file
 logger = logging.getLogger(__name__)
 
 
+def _002_remove_orphan_image(session: Session):
+    images = session.exec(select(Image)).all()
+    if not images:
+        return
+
+    db_image_filenames = {img.filename for img in images if img.filename}
+    assets_dir = Path(get_settings().ASSETS_FOLDER)
+    orphans = 0
+    for fp in assets_dir.iterdir():
+        if not fp.is_file():
+            continue
+
+        if fp.name not in db_image_filenames:
+            try:
+                fp.unlink()
+                orphans += 1
+            except Exception as exc:
+                logger.error(
+                    f"[Migration 002_remove_orphan_image] Error while removing orphan image {fp.name}. Error: {exc}"
+                )
+
+    if orphans:
+        logger.warn(f"[Migration 002_remove_orphan_image] Removed {orphans} orphan images")
+
+
 def _001_image_file_size(session: Session):
     images = session.exec(select(Image).where((Image.file_size.is_(None)) | (Image.file_size == 0))).all()
     if not images:
         return
 
     dst = backup_file(Path(get_settings().SQLITE_FILE))
-    logger.warn(f"[Migration] Database backed up to {dst} before changes")
+    logger.warn(f"[Migration 001_image_file_size] Database backed up to {dst} before changes")
 
     assets = Path(get_settings().ASSETS_FOLDER)
     for image in images:
@@ -29,3 +54,4 @@ def _001_image_file_size(session: Session):
 
 def run_migrations(session: Session):
     _001_image_file_size(session)
+    _002_remove_orphan_image(session)
